96f0eb1693623f7d32c0379cff4ddbe2f7037392
Saurabh Nakra <saurabhnakra95@gmail.com>
2016-05-21 13:22:56 +0530

    Polls app

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls import include, url
+from django.contrib import admin
+
 urlpatterns = [
+	url(r'^polls/', include('polls.urls')),
+	url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list, name='post_list'),
 ]
